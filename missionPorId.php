2cca8a5206376fefdbdad50d63c13ae4fe26c21d
Pau Salto <pausaltom@gmail.com>
2021-02-17 18:57:23 +0100

    Toque final

--- missionPorId.php
+++ missionPorId.php
@@ -1,41 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $mysqli= new mysqli("localhost", "root", "", "missions");
-        $ident=$_GET['number'];
+    $mysqli = new mysqli("localhost", "root", "", "missions");
+    $ident = $_GET['number'];
 
-        if($mysqli->connect_errno){
-            echo("Connect failed: ".$mysqli->connect_error);
-            exit();
-        }
+    if ($ident==null) {
+        $ident = 99999999;
+    }
+    
+    if ($mysqli->connect_errno) {
+        echo ("Connect failed: " . $mysqli->connect_error);
+        exit();
+    }
 
-        $result=$mysqli->query("SELECT * from quests WHERE id=$ident");
-       
-        echo("<b>Mission ".$ident." : </b>");
-        echo("</br>");
-        
-        $row = $result->fetch_object(); /*LEE ROW Y AVANZA*/ 
+    $result = $mysqli->query("SELECT * from quests WHERE id=$ident");
 
-        echo($row->id." / ".$row->descripcion." / ".$row->num_vots." / ".$row->mitjana."<br/>");
-        
+    if (mysqli_num_rows($result) != 0) {
+
+        echo ("<b>Mission " . $ident . " : </b>");
+        echo ("</br>");
+
+        $row = $result->fetch_object(); /*LEE ROW Y AVANZA*/
+
+        echo ($row->id . " / " . $row->descripcion . " / " . $row->num_vots . " / " . $row->mitjana . "<br/>");
+
         $result->free();
         $mysqli->close();
+    } else {
+        echo ('<b>No existe un usuario con este id</b>');
+    }
+
     ?>
     </br>
     </br>
     <button id="mainPage"> Pagina principal </button>
-    
+
     <script type="text/javascript">
         document.getElementById("mainPage").addEventListener("click", goBack);
-        function goBack(){
-            window.location="./index.html";
+
+        function goBack() {
+            window.location = "./index.html";
         }
     </script>
 </body>
+
 </html>
--- usuarioPorId.php
+++ usuarioPorId.php
@@ -1,41 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $mysqli= new mysqli("localhost", "root", "", "missions");
-        $ident=$_GET['numberUsu'];
+    $mysqli = new mysqli("localhost", "root", "", "missions");
+    $ident = $_GET['numberUsu'];
+    if ($ident==null) {
+        $ident = 999999999;
+    }
+    if ($mysqli->connect_errno) {
+        echo ("Connect failed: " . $mysqli->connect_error);
+        exit();
+    }
 
-        if($mysqli->connect_errno){
-            echo("Connect failed: ".$mysqli->connect_error);
-            exit();
-        }
+    $result = $mysqli->query("SELECT * from users WHERE id=$ident");
 
-        $result=$mysqli->query("SELECT * from users WHERE id=$ident");
+    if(mysqli_num_rows($result) != 0) {
+        echo ("<b>Usuario " . $ident . " : </b>");
+        echo ("</br>");
 
-        echo("<b>Usuario ".$ident." : </b>");
-        echo("</br>");
+        $row = $result->fetch_object(); /*LEE ROW Y AVANZA*/
 
-        $row = $result->fetch_object(); /*LEE ROW Y AVANZA*/ 
-
-        echo($row->id." / ".$row->name." / ".$row->surname." / ".$row->nick." / ".$row->password." / ".$row->score."<br/>");
-        
+        echo ($row->id . " / " . $row->name . " / " . $row->surname . " / " . $row->nick . " / " . $row->password . " / " . $row->score . "<br/>");
         $result->free();
         $mysqli->close();
+    } else {
+        echo ('<b>No existe una misión con este id</b>');
+    }
+
+
     ?>
     </br>
     </br>
     <button id="volverIndex"> Pagina principal </button>
-    
+
     <script type="text/javascript">
         document.getElementById("volverIndex").addEventListener("click", volver);
-        function volver(){
-            window.location="./index.html";
+
+        function volver() {
+            window.location = "./index.html";
         }
     </script>
 </body>
+
 </html>
